0e079d08c288e36a3087139f1c681ce66c9a9d37
Patricia Reghine <86622801+PatriciaReghine@users.noreply.github.com>
2025-07-30 22:24:51 -0300

    Add files via upload

---
+++ playStore/src/test/java/com/playstore/playstore/PlayStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playstore.playstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
